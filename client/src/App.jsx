08fdaa98d4ab53a3358d03ecd4d10156839d8e46
RahulPali78 <palisettirahul123@gmail.com>
2025-03-08 22:41:52 +0000

    Initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <div className="container bg-red-50"></div>;
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//create a database connection  -->u can also
+//create separate file for this and import or use that file here
+
+mongoose
+  .connect(
+    "mongodb+srv://krishnaveni123:krishnaveni1011@cluster0.btvzf.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running a PORT ${PORT}`));
