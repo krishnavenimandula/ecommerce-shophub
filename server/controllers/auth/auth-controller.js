099c672598d578a418021f65b35b8350f38baa4e
krishnavenimandula <krishnavenimandula123@gmail.com>
2025-04-07 22:48:37 +0100

    Validated login and registration

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -7,6 +7,13 @@ const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
 
   try {
+    //checking user existed or not
+    const checkUser = await User.findOne({ email });
+    if (checkUser)
+      return res.json({
+        success: false,
+        message: "User already exists with the same email.Please try again",
+      });
     const hashPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
       userName,
@@ -30,10 +37,48 @@ const registerUser = async (req, res) => {
 
 //login
 
-const login = async (req, res) => {
+const loginUser = async (req, res) => {
   const { email, password } = req.body;
-
+  console.log(req);
   try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser)
+      return res.json({
+        success: false,
+        message: "User doesn't exist! Please register first",
+      });
+
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
+    if (!checkPasswordMatch)
+      return res.json({
+        success: false,
+        message: "Invalid Password! Please try again",
+      });
+
+    // creating token
+    const token = jwt.sign(
+      {
+        id: checkUser._id,
+        role: checkUser.role,
+        email: checkUser.email,
+      },
+      "CLIENT_SECRET_KEY",
+      { expiresIn: "60m" }
+    );
+
+    // saving in cookies
+    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+      success: true,
+      message: "Logged in successfully!",
+      user: {
+        role: checkUser.role,
+        email: checkUser.email,
+        id: checkUser._id,
+      },
+    });
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -45,6 +90,13 @@ const login = async (req, res) => {
 
 //logout
 
+const logout = (req, res) => {
+  res.clearCookie("token").json({
+    success: true,
+    message: "Logged out successfully!",
+  });
+};
+
 // auth-middleware
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginUser, logout };
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5177",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
