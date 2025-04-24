8c9ca74f9e16b015dccec09d2757e3bd17b8c69d
krishnavenimandula <krishnavenimandula123@gmail.com>
2025-04-24 23:57:58 +0100

    shopping header

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,5 +1,9 @@
 function ShoppingHeader() {
-  return <div>Shopping header</div>;
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6"></div>
+    </header>
+  );
 }
 
 export default ShoppingHeader;
