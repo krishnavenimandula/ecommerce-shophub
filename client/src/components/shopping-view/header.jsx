47d3d2467feaa5c4134faa93e0d96469c4bc8ec2
krishnavenimandula <krishnavenimandula123@gmail.com>
2025-04-25 22:39:29 +0100

    shopping view header component implemeted.

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,7 +1,90 @@
+import { Link } from "react-router-dom";
+import { Menu, ShoppingCart } from "lucide-react";
+
+import { HousePlug } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "@/config";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
+
+function MenuItems() {
+  return (
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+      {shoppingViewHeaderMenuItems.map((menuItem) => (
+        <Link
+          className="text-sm font-medium"
+          key={menuItem.id}
+          to={menuItem.path}
+        >
+          {menuItem.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
+
+function HeaderRightContent() {
+  // const { user } = useSelector((state) => state.auth);
+  // console.log(user);
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+      <Button variant="outline" size="icon">
+        <ShoppingCart className="w-6 h-6" />
+        <span className="sr-only">User cart</span>
+      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="bg-black rounded">
+            <AvatarFallback className="bg-black text-white font-extrabold">
+              KM
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="right" className="w-56">
+          <DropdownMenuLabel>Logged in as</DropdownMenuLabel>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
 function ShoppingHeader() {
+  const { isAuthenticated } = useSelector((state) => state.auth);
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="flex h-16 items-center justify-between px-4 md:px-6"></div>
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+        {isAuthenticated ? (
+          <div>
+            <HeaderRightContent />
+          </div>
+        ) : null}
+      </div>
     </header>
   );
 }
